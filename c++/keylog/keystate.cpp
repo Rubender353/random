c4b8d7ffd2cdb3da4ff05a2d9a456767fdd6ae6c
Rubender353 <31175996+Rubender353@users.noreply.github.com>
2025-07-20 20:24:37 -0700

    Update keystate.cpp

--- c++/keylog/keystate.cpp
+++ c++/keylog/keystate.cpp
@@ -13,3 +13,47 @@ int keystate()
     // to do map of other keys
     std::map<std::string, std::string> otherKeys { {0x30, ")"},
 }
+
+//TODO: this method uses a struct to hold regular and shift keys and stores it in one unordered_map called keyMap
+/*
+#include <iostream>
+#include <unordered_map>
+#include <string>
+
+// Struct to hold normal and shifted key representations
+struct KeyRepresentation {
+    std::string normal;
+    std::string shifted;
+};
+
+int main() {
+    std::unordered_map<int, KeyRepresentation> keyMap {
+        {0x30, {"0", ")"}},  // 0 key: normal "0", shifted ")"
+        {0x31, {"1", "!"}},  // 1 key
+        {0x32, {"2", "@"}},  // 2 key
+        {0x33, {"3", "#"}},
+        {0x34, {"4", "$"}},
+        {0x35, {"5", "%"}},
+        {0x36, {"6", "^"}},
+        {0x37, {"7", "&"}},
+        {0x38, {"8", "*"}},
+        {0x39, {"9", "("}},
+        // Add other keys as needed
+    };
+
+    int keycode = 0x31; // For example: key '1'
+    bool shiftPressed = true;
+
+    auto it = keyMap.find(keycode);
+    if (it != keyMap.end()) {
+        if (shiftPressed)
+            std::cout << "Shifted key: " << it->second.shifted << "\n";
+        else
+            std::cout << "Normal key: " << it->second.normal << "\n";
+    } else {
+        std::cout << "Keycode not found\n";
+    }
+
+    return 0;
+}
+*/
